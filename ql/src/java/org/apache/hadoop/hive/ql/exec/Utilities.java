dda729c4a93519e423020d1ddbda06fad17585fa
Peter Toth <peter.toth@gmail.com>
2019-07-31 20:26:06 +0200

    Revert "[BUG-121132]Revert "Revert "[BUG-59263][HIVE-13931] SUMMARY-[Update connection pool usage from bonecp to hikaricp]"""
    This reverts commit 7bc95f2ada6bb865508c1dd5feecf9bd13094ab9.

--- ql/src/java/org/apache/hadoop/hive/ql/exec/Utilities.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/Utilities.java
@@ -1044,8 +1044,6 @@ public final class Utilities {
 
     e.setPersistenceDelegate(org.datanucleus.store.types.wrappers.backed.Map.class, new MapDelegate());
     e.setPersistenceDelegate(org.datanucleus.store.types.wrappers.backed.List.class, new ListDelegate());
-    e.setPersistenceDelegate(org.datanucleus.store.types.wrappers.Map.class, new MapDelegate());
-    e.setPersistenceDelegate(org.datanucleus.store.types.wrappers.List.class, new ListDelegate());
     e.setPersistenceDelegate(CommonToken.class, new CommonTokenDelegate());
     e.setPersistenceDelegate(Path.class, new PathDelegate());
 
